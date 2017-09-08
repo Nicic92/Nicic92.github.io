08be9abcfe86aaeb237cf12a7509813255e71e11
flash <nicic92@gmail.com>
2017-09-08 18:55:51 +0200

    added hashing removed font

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,70 @@
+/* 
+intall Node.js
+move package.json gulpfile.js
+npm init
+npm install
+
+file structure :
+src/css   -----\
+src/js 			---> goes to root 
+src/img   -----/
+src/.html ----/
+
+
+-------------------- TODO --------------------
+
+Lazyload CSS to lazy to include it: 
+Critical CSS 	 --- https://github.com/addyosmani/Critical 	- minimal css to make page apear normal
+loadcss 		 --- https://github.com/filamentgroup/loadCSS 	- asynchronosly load css
+gulp-rev 		 --- https://github.com/sindresorhus/gulp-rev 	- Static asset revisioning by appending content hash to filenames: unicorn.css → unicorn-d41d8cd98f.css
+hash file names 
+
+Commands:
+
+			----------- Single tasks ------------
+No concats here
+npm run update  --- Updates Dependacies
+gulp htmlmin 	--- Minimise html
+gulp html 		--- Copy html from dev to prod
+gulp fonts  	--- Copies Fonts from dev to prod
+gulp sass 		--- Compile Sass and place it into same CSS folder 
+gulp css 		--- Remove unused, autoprefix, minimize
+					Note: update gulp.task css: html[] ignore dynamic properies
+gulp js 		--- Babel, minimize
+gulp images 	--- Compress images Wont repeat if image is compresed
+gulp panda 		--- Best Image compresion 500/mo/key, Wont repet
+
+
+gulp clean		--- Remove JS and CSS files from build folder
+gulp devserver	--- Runs live server, resourses from src folder, Does not reload
+gulp prodserver --- Runs live server, resourses from prod folder, Does not reload
+gulp ngrok 		--- Live website NOTE: run ngrok program, type: ngrok http 8001
+
+			----------- Multi tasks ------------
+
+gulp run 		--- Runs gulp html css js fonts images
+gulp watch 		--- Uses browserSync server Watches for changes reloads page, compiles SASS changes on save to CSS
+
+gulp concat		--- Runs LAZYPIPE: css, js WITH concatination and Hashing
+					Note: in HTML:
+					<!-- build:css css/main.css -->
+					<!-- endbuild -->
+					<!-- build:js  js/main.css -->
+					<!-- endbuild -->
+
+*/
+
 var
 	gulp = require('gulp'),
+	rename = require("gulp-rename"),
 	imagemin = require('gulp-imagemin'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync').create(),
-	useref = require('gulp-useref'),  //gulp-concat if this fucks up (must manualy add files) (good for constant gulp runs js/css (prod))
+	useref = require('gulp-useref'),  //use gulp-concat if this fucks up (must manualy add files) (good for constant gulp runs js/css (prod))
 	gulpif = require('gulp-if');
-	uglify = require('gulp-uglify');
-	csso = require('gulp-csso');
+	uglify = require('gulp-uglify'); //ES5 minifier
+	uglify6 = require('gulp-uglify-es').default; //ES6
+	csso = require('gulp-csso'); //minify
 	babel = require('gulp-babel');
 	plumber = require('gulp-plumber');
 	uncss = require('gulp-uncss');
@@ -17,6 +75,10 @@ var
 	tinypng = require('gulp-tinypng');
 	changed = require('gulp-changed');
 	connect = require('gulp-connect'); //for ngrok server (build)
+	rev = require('gulp-rev'); //hash plugin
+	revReplace = require('gulp-rev-replace'); //hash plugin
+	del = require('del');
+
 
 
 	folder = {
@@ -30,12 +92,16 @@ var
 
 	//HTML
 	gulp.task('htmlmin', function() {
-	  	return gulp.src(folder.build +'./*.html')
+	  	return gulp.src(folder.src +'./*.html')
 	  	.pipe(plumber())
 	    .pipe(htmlmin({collapseWhitespace: true}))
 	    .pipe(gulp.dest(folder.build));
 	});
 
+	gulp.task('html', function() {
+		return gulp.src(folder.src + './*.html')
+		.pipe(gulp.dest(folder.build))
+	})
 
 	//SASS
 	gulp.task('sass', function () {
@@ -55,25 +121,28 @@ var
 	return gulp.src(folder.src + 'css/**/*.css')
 	.pipe(plumber())
 	.pipe(uncss({				
-            html: [folder.build + 'index.html'],
+            html: [folder.src + 'index.html'],
+            // html: [folder.build + '*.html'],
             ignore: ['.fadeIn', '.fadeInDown','.fadeInRight','.fadeInLeft',
             		 '.fadeInUp','.animated','.zoomIn']
         }))
 	.pipe(postcss([ autoprefixer() ]))
-	// .pipe(csso())
+	.pipe(csso())
+	// .pipe(rev()) hashing file
 	.pipe(gulp.dest(out));
 	});
 
 
 	//JS
 	gulp.task('js', function () {
-	    gulp.src(folder.src + 'js/*.js')
+		var out = folder.build + 'js/';
+	    return gulp.src(folder.src + 'js/**/*.js')
 	    .pipe(plumber())
-	    .pipe(babel({presets: ['es2015']}))
-	    // .pipe(uglify())
-	    gulp.dest(folder.build + 'js/')
+	    .pipe(babel({presets: ['es2016']}))
+	    .pipe(uglify6())
+	    // .pipe(rev()) hashing file
+	    .pipe(gulp.dest(out));
 	});
-
 
 	//IMAGES
 		gulp.task('images', function() {
@@ -100,56 +169,59 @@ var
 	gulp.task('fonts', function() {
 		return gulp.src(folder.src + './fonts/**/*')
 		.pipe(gulp.dest(folder.build + 'fonts/'))
-	})
+	});
+
+	gulp.task('clean', function () {
+    	del([folder.build + './js/**/*', 
+    		 folder.build + './css/**/*' ])
+    	
+	});
 
 
 	// Lazypipe TASKS
-	var cssTasks = lazypipe()
-		.pipe(plumber)
-		.pipe(uncss,{ html: [folder.build + 'index.html'],//,'http://localhost:8080/portfolioG/dist/ (for runtime css added through js)
-					  ignore:['.fadeIn', '.fadeInDown','.fadeInRight','.fadeInLeft',			// 	 (for added css through user interaction)
-            		 '.fadeInUp','.animated','.zoomIn'] 
-            		})
-		.pipe(postcss,[autoprefixer])
-		.pipe(csso)
-    ;
-
-    var jsTasks = lazypipe()
-		.pipe(plumber)
-		.pipe(uglify)
-		
-    ;
-
-    //FOR DIST, HARD TO DEBUG
-	//HTML CSS JS
-
-	//		useref
-	//		<!-- build:css css/main.css -->		(build:js)
-	//		<!-- endbuild -->
 
 	//		lazypipe
 	//.pipe(foo(param1)) 		 -> .pipe(foo, param1)
 	//.pipe(foo(param1, param2)) -> .pipe(foo, param1, param2)
 
-	gulp.task('all', function () {
+	var cssTasks = lazypipe()
+		.pipe(plumber)
+		.pipe(uncss,{ html: [folder.src + 'index.html'],//,'http://localhost:8080/portfolioG/dist/ (for runtime css added through js)
+					  ignore:['.fadeIn', '.fadeInDown','.fadeInRight','.fadeInLeft',			// 	 (for added css through user interaction)
+            		 '.fadeInUp','.animated','.zoomIn'] 
+            		})
+		.pipe(postcss,[autoprefixer])
+		.pipe(csso)
+		.pipe(rev)
+    ;
+
+    var jsTasks = lazypipe()
+		.pipe(plumber)
+		.pipe(babel,{presets: ['es2016']})
+		.pipe(uglify6)
+		.pipe(rev)
+    ;
+
+
+	gulp.task('concat', function () {
 	return gulp.src(folder.src +'./*.html')
 		//handle errors
 		.pipe(plumber())
 		// handle file concatenation 
 		.pipe(useref())
-		//handle minification (Conditionally run a task)
+		//handle tasks
 		.pipe(gulpif('*.js', jsTasks()))
 		.pipe(gulpif('*.css', cssTasks()))
-
+		.pipe(revReplace())
 		.pipe(gulp.dest(folder.build));
 	});
 
 
 
-	gulp.task('run', ['html', 'css', 'js', 'fonts']);
+	gulp.task('run', ['html', 'css', 'js', 'fonts', 'images']);
 
-	//WATCH DEV
-	gulp.task('watch', ['browserSync', 'sass'], function () {
+	//WATCH DEV CANT WAIT TO COMPILE
+	gulp.task('watch', ['devserver', 'sass'], function () {
 	gulp.watch(folder.src +'./css/**/*.scss', ['sass']);
 	gulp.watch(folder.src +'./css/**/*.css', browserSync.reload);
 	gulp.watch(folder.src +'./js/**/*.js', browserSync.reload);
@@ -157,16 +229,23 @@ var
 	});
 
 
-	gulp.task('browserSync', function() {
+	gulp.task('devserver', function() {
 	browserSync.init({
 	server: {
-	baseDir: folder.dest
+	baseDir: folder.src
+	},})
+	});
+
+	gulp.task('prodserver', function() {
+	browserSync.init({
+	server: {
+	baseDir: folder.build
 	},})
 	})
 
-	gulp.task('ngrok', function () {  //run ngrok, type: ngrok http 8001
+	gulp.task('ngrok', function () {
 	connect.server({
-	root: folder.src,
+	root: folder.build,
 	port: 8001,
 	livereload: false
 	});
